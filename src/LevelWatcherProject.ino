c683c6fc61cdf417a113ed5551d1bf879bc1b196
Patrick Fischer <pfischer@hotmail.co.uk>
2021-08-18 09:30:39 +0100

    Fixed title description

--- src/LevelWatcherProject.ino
+++ src/LevelWatcherProject.ino
@@ -1,6 +1,6 @@
 //Version info
 
-//This is branch V2 ... Working reference branch. Only apply bug fixes etc to this branch.
+//This is branch master ... WIP
 //Includes startup call to get and set zero offset in mm.
 // This is done by publishing a startup event which triggers a function call to the device that includes
 //the zero offest as a parameter in the function call.
